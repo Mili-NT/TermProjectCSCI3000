b0e54cfbd10c415e086eb1e3434dce3ac74ef724
Brendan <33934145+BrendanoElTaco@users.noreply.github.com>
2023-04-20 14:25:20 -0400

    Added js to message box

--- javascript_test_lauren.js
+++ javascript_test_lauren.js
@@ -1,1 +1,10 @@
-//put this here just in case we need it
+const tx = document.getElementsByTagName("textarea");
+    for (let i = 0; i < tx.length; i++) {
+      tx[i].setAttribute("style", "height:" + (tx[i].scrollHeight) + "px;overflow-y:hidden;");
+      tx[i].addEventListener("input", OnInput, false);
+    }
+    
+    function OnInput() {
+      this.style.height = 0;
+      this.style.height = (this.scrollHeight) + "px";
+    }
