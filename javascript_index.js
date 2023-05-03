43fa17bc915816505174838b2a86be76c2abe8c1
Mili <mili@mili.mil>
2023-05-03 18:38:16 -0400

    Added AJAX for contact (oops lmao) and made cart function for localstorage req

--- javascript_index.js
+++ javascript_index.js
@@ -1,10 +1,19 @@
 const tx = document.getElementsByTagName("textarea");
-    for (let i = 0; i < tx.length; i++) {
-      tx[i].setAttribute("style", "height:" + (tx[i].scrollHeight) + "px;overflow-y:hidden;");
-      tx[i].addEventListener("input", OnInput, false);
-    }
-    
-    function OnInput() {
-      this.style.height = 0;
-      this.style.height = (this.scrollHeight) + "px";
-    }
+for (let i = 0; i < tx.length; i++) {
+  tx[i].setAttribute("style", "height:" + (tx[i].scrollHeight) + "px;overflow-y:hidden;");
+  tx[i].addEventListener("input", OnInput, false);
+}
+
+function OnInput() {
+  this.style.height = 0;
+  this.style.height = (this.scrollHeight) + "px";
+}
+function addToCart(itemPrice, itemName) {
+    const cartItem = {
+        price: itemPrice,
+        name: itemName
+    };
+    const cart = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : [];
+    cart.push(cartItem);
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
